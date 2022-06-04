e397b8d8e0aa4b95cbcbb17eb9092cc5306696c4
ramonlopz1 <rmnprojetos@outlook.com>
2022-06-03 21:49:00 -0300

    section slider function

--- src/assets/js/dom/home/banners-inputs-radios.js
+++ src/assets/js/dom/home/banners-inputs-radios.js
@@ -2,8 +2,27 @@
     const btnsRadios = document.querySelectorAll('#btns__radios');
     let contador = 0;
 
+
+    // soma + 1 para que o loop aconteça.
+    setInterval(() => {
+        checkBtnRadio(contador);
+        changeBackGround(contador)
+        contador++;
+
+        contador >= 3 ? contador = 0 : contador;
+    }, 3500)
+
+    // altera o contador para o índice do botão clicado, mudando a animação.
+    btnsRadios.forEach((btn, idx) => {
+        btn.addEventListener('click', () => {
+            contador = idx;
+            checkBtnRadio(contador);
+            changeBackGround(contador);
+        })
+    })
+    
     // Looping para dar checked e unchecked no btnsRadios
-    const checkedLoop = (contador) => {
+    const checkBtnRadio = (contador) => {
         btnsRadios.forEach((btn, idx) => {
             if (contador !== idx) {
                 btn.checked = false
@@ -13,18 +32,8 @@
         })
     }
 
-
-    setInterval(() => {
-        checkedLoop(contador);
-        contador++;
-
-        contador >= 3 ? contador = 0 : contador;
-    }, 1000)
-
-    btnsRadios.forEach((btn, idx) => {
-        btn.addEventListener('click', () => {
-            contador = idx
-            checkedLoop(contador)
-        })
-    })
-})()
+    const changeBackGround = (contador) => {
+       const bg = document.querySelector("#home__main__banners");
+       bg.style.backgroundImage = `url('../assets/img/home/banners/first_banner/${contador}.jpg')`
+    }
+})();
---
+++ src/assets/js/dom/home/slider-btns.js
@@ -0,0 +1,24 @@
+(() => {
+    const wrapperDiv = document.querySelector("#slider__element__list");
+        wrapperDiv.style.transition = '.6s';
+    let translateValue = 0;
+
+    document.querySelectorAll(".slider__btn")
+        .forEach((btn, idx) => {
+            btn.addEventListener("click", event => {
+                event.preventDefault();
+                move(idx);
+            })
+        });
+
+    const move = (btn) => {
+        if (btn === 0 && translateValue <= -130) {
+            translateValue = translateValue + 130;
+            wrapperDiv.style.transform = `translateX(${translateValue}px)`;
+
+        } else if (btn === 1 && translateValue <= 0 && translateValue > -1300) {
+            translateValue = translateValue + (-130);
+            wrapperDiv.style.transform = `translateX(${translateValue}px)`;
+        }
+    }
+})();
